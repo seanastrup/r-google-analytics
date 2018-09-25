73b415cdd10009256514efa06b5511525a672f69
Sean Astrup <13409547+seanastrup@users.noreply.github.com>
2018-09-24 22:24:13 -0500

    Materialize this guy. Should have made multiple commits.

--- app.R
+++ app.R
@@ -10,39 +10,94 @@ library(highcharter)
 source('ga-auth.R')
 
 
+
+
+
+
 ui <- 
-  dashboardPage(
-    dashboardHeader(
-      title = 'Google Analytics R'
-    ),  # dashboard header
-    dashboardSidebar(
-      width = 350,
-      sidebarMenu(
-        menuItem(
-          googleAuthUI("login")
-        )  # menu item
-      ),  # sidebar menu
-      authDropdownUI("auth_menu")
-    ),  #dashboard sidebar
-    dashboardBody(
-      highchartOutput("something")
-    )
-  )  # dashboard page
+  shiny::fluidPage(
+    tags$head(
+      # tags$script(src = 'jquery-3.3.1.min.js'),  # jquery
+      tags$link(rel = 'stylesheet', href = 'https://fonts.googleapis.com/icon?family=Material+Icons'), # google fonts
+      tags$link(rel = 'stylesheet', href = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css'),  # material css
+      tags$script(src = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js'),  # material js
+      tags$style(HTML('
+                      body {
+                      background-color:#fafafa;
+                      }
+                      .selectize-input{
+                        height:50%;
+                      }'
+          )
+      ),
+      tags$script(HTML("
+        $(document).ready(function(){
+                       $('.sidenav').sidenav();
+                       });"
+        )
+      )
+    ),  # head
+    tags$nav(class = 'blue darken-3',
+      tags$div(class = 'nav-wrapper',
+        tags$a(href = '#', class = 'brand-logo',
+          'Google Analytics - R'
+        )
+      )  # nav wrapper
+    ),   # navbar
+    tags$br(),
+    tags$ul(id = 'slide-out', class = 'sidenav', 
+      tags$li(
+        tags$div(class = 'user-view', 
+          tags$div(class = 'background'
+            # insert packground
+          ), 
+          tags$br()
+        )
+      ),  # background li  
+      tags$div(class = 'card z-debth-5',
+      tags$div(class = 'card-content',
+               'TEST'
+          # googleAuthUI('login'),
+          # authDropdownUI('auth_menu')
+        )  # card content
+      )  # card
+    ),  # side nav
+    shiny::fluidRow(
+      shiny::column(
+        width = 2,
+        tags$div(class = 'card-z-debth-5', 
+          tags$div(class = 'card-content',
+            googleAuthUI('login'),
+            authDropdownUI('auth_menu')
+          )  # card content
+        )   # card
+      ),  # column
+      shiny::column(
+        width = 6,
+        tags$div(class = 'card z-debth-5', 
+          tags$div(class = 'card-content',
+            highcharter::highchartOutput('DailyDeviceSessions')       
+          )  # card content
+        )  # card
+      )  # column
+    )  # row
+  )  # ui
 
 
 
 server <- function(input, output) {
 
-  token <- callModule(googleAuth, "login")
+  token <- callModule(googleAuth, 'login')
   
   ga_accounts <- reactive({
+    
     req(token())
     
     with_shiny(ga_account_list, shiny_access_token = token())
     
   })
   
-  selected_id <- callModule(authDropdown, "auth_menu", ga.table = ga_accounts)
+  selected_id <- callModule(authDropdown, 'auth_menu', ga.table = ga_accounts)
   
   gadata <- reactive({
     
@@ -50,21 +105,24 @@ server <- function(input, output) {
     gaid <- selected_id()
     with_shiny(google_analytics,
                viewId = gaid,
-               date_range = c("2018-06-01", "2018-07-02"),
-               metrics = c("sessions"),
-               dimensions = c("date"),
+               date_range = c(lubridate::today() - 60, lubridate::today()),
+               metrics = c('sessions'),
+               dimensions = c('date'),
                shiny_access_token = token())
+    
   })
   
-  output$something <- renderHighchart({
+  output$DailyDeviceSessions <- renderHighchart({
     
-    ## only trigger once authenticated
+    # only trigger once authenticated
     req(gadata())
     
     gadata <- gadata()
     
-    ## creates a line chart using highcharts
-    hchart(gadata, "line" , hcaes(x = date, y = sessions))
+    highcharter::hchart(gadata, 'spline' , hcaes(x = date, y = sessions 
+                                                 )) %>%
+      hc_title(text = 'Device Sessions')
+      
     
   })
 }
